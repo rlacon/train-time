2f80e1813449bf75c975387e5d70e6578a3d83aa
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-13 09:45:57 -0400

    Set up html and javascript files

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyABqAZU7UeVQ7nA16mkhFxcmjTSM9cXKJA",
+    authDomain: "dogwood-terra-183816.firebaseapp.com",
+    databaseURL: "https://dogwood-terra-183816.firebaseio.com",
+    projectId: "dogwood-terra-183816",
+    storageBucket: "dogwood-terra-183816.appspot.com",
+    messagingSenderId: "416717539320",
+    appId: "1:416717539320:web:5cabe2f014923c29"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+$(document).ready(function () {
+    console.log("Document Loaded!");
+
+
+var database = firebase.database();
+
+
+
+
+
+});
